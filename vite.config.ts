67f1ca27a229992e247b31465ee98821377b61df
wuchuheng <root@wuchuheng.com>
2022-01-28 16:29:51 +0800

    feat: configurature eslint, tsconfig and vite .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: /^~/, replacement: '' }]
+    alias: [
+      { find: /^~/, replacement: '' },
+      { find: '@/*', replacement: './src/*' },
+      { find: '@/styles/*', replacement: './src/styles/*' }
+    ]
   }
 })
