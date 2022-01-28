bd2f152fc9368f3199deb5b5f309b6115b79926f
wuchuheng <root@wuchuheng.com>
2022-01-28 18:21:21 +0800

    feat: installed react-icons.

--- src/container/Layout/index.tsx
+++ src/container/Layout/index.tsx
@@ -1,7 +1,65 @@
 import React from "react";
+import style from "./style.module.less"
+import {IoHomeOutline, IoLogoApple, IoPeopleOutline,
+  IoChatbubbleOutline,
+  IoHelpOutline,
+  IoSettingsOutline,
+  IoLockClosedOutline,
+  IoLogOutOutline
+
+} from "react-icons/io5";
 
 const Layout: React.FC = () => {
-  return (<div>layout</div>)
+  return (<div className={style.main}>
+    <div className={style.navigation}>
+      <ul>
+        <li>
+          <a href="#"></a>
+           <span className={style.icon}> <IoLogoApple /> </span>
+          <span className={style.title}>
+            Brand Name
+          </span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}> <IoHomeOutline /> </span>
+          <span className={style.title}>
+            Dashboard</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}> <IoPeopleOutline /> </span>
+          <span className={style.title}> Customers</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}><IoChatbubbleOutline /></span>
+          <span className={style.title}>Messages</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}><IoHelpOutline /></span>
+          <span className={style.title}>Helpe</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}><IoSettingsOutline /></span>
+          <span className={style.title}>Settings</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}><IoLockClosedOutline /></span>
+          <span className={style.title}>Sign out</span>
+        </li>
+        <li>
+          <a href="#"></a>
+          <span className={style.icon}><IoLogOutOutline /></span>
+          <span className={style.title}>Sign out</span>
+        </li>
+
+      </ul>
+    </div>
+  </div>)
 }
 
 export default Layout
